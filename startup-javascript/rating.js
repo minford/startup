e70f39bc9c0c90690fbc5f48c6e2124470b38ae7
Minami <minami.ford@byu.net>
2024-03-09 18:33:36 -0700

    service updates

--- startup-javascript/rating.js
+++ startup-javascript/rating.js
@@ -1,35 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <title>Lavocado HTML</title>
-  <link rel="icon" href="Logo.jpg" />
-  <script src="login.js"></script>
-  <link rel="stylesheet" href="main.css" />
-</head>
-
-<body>
-  <!-- Use header, main, and footer elements to give semantic structure -->
-  <header>
-    <h1>Lavocado<sup>&reg;</sup></h1>
-    <div id="picture" class="picture-box">
-      <img src="Logo.jpg" alt="random"/>
-    </div>
-  </header>
-
-  <main>
-    <div>
-      
-    </div>
-  </main>
-
-  <footer>
-    <span>Minami Ford</span>
-    <br />
-    <a href="https://github.com/minford/startup/tree/main/startup-html">GitHub</a>
-  </footer>
-
-</body>
-
-</html>
+document.getElementById("RateBtn").addEventListener("click", function() {
+  document.getElementById("recipeForm").style.display = "block";
+});

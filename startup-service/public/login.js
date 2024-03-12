0e4249279d0497d77e8fdb6bbf59646b9665bc55
Minami <minami.ford@byu.net>
2024-03-11 22:11:45 -0600

    add third party api

--- startup-service/public/login.js
+++ startup-service/public/login.js
@@ -5,3 +5,24 @@ function login() {
     localStorage.setItem("passWord", passwordEl.value);
     window.location.href = "recipes.html";
   }
+
+  function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+  }
+  
+  displayQuote();

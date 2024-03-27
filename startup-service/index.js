d464d23c5f93402dae1081c9f6059dd9536c837b
Minami <minami.ford@byu.net>
2024-03-27 11:46:11 -0600

    fix rating

--- startup-service/index.js
+++ startup-service/index.js
@@ -16,55 +16,59 @@ app.use(`/api`, apiRouter);
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-  });
-  
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-  
-  //keep track of recipe lists
-  let recipes = [];
-  let savedRecipes = [];
+  res.sendFile('index.html', { root: 'public' });
+});
 
-  apiRouter.post('/recipes', (req, res) => {
-    const newRecipe = req.body;
-    recipes.push(newRecipe);
-    res.send(recipes);
-  });
-  
-  apiRouter.get('/recipes', (_req, res) => {
-    console.log(recipes);
-    res.send(recipes);
-  });
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
-  apiRouter.post('/saved-recipes', (req, res) => {
-    const newRecipe = req.body;
-    savedRecipes.push(newRecipe);
-    res.send(savedRecipes);
-  });
-  
-  apiRouter.get('/saved-recipes', (_req, res) => {
-    res.send(savedRecipes);
-  });
-  
-  apiRouter.post('/rate', (req, res) => {
-    const recipeIndex = recipes.findIndex(recipe => recipe.name === req.body.recipeName);
-    //console.log(req.body.rating);
-    if (recipeIndex !== -1) {
-      // Add the rating to the recipe's ratings array
-      recipes[recipeIndex].ratings.push(req.body.rating);
-      //console.log(recipes[recipeIndex].ratings);
-  
-      // Calculate the average rating for the recipe
-      const totalRating = recipes[recipeIndex].ratings.reduce((acc, curr) => acc + curr, 0);
-      console.log(totalRating);
-      const averageRating = totalRating / recipes[recipeIndex].ratings.length;
-      console.log(averageRating);
-  
-      res.json({ success: true, averageRating });
-    } else {
-      res.status(404).json({ success: false, message: 'Recipe not found' });
-    }
-  });
-  //TODO: Make sure averageRating is getting the right number and update recipe.rating to be the average rating so that it shows on the recipes page and single recipes page correctly
+//keep track of recipe lists
+let recipes = [];
+let savedRecipes = [];
+
+apiRouter.post('/recipes', (req, res) => {
+  const newRecipe = req.body;
+  recipes.push(newRecipe);
+  res.send(recipes);
+});
+
+apiRouter.get('/recipes', (_req, res) => {
+  //console.log(recipes);
+  res.send(recipes);
+});
+
+apiRouter.post('/saved-recipes', (req, res) => {
+  const newRecipe = req.body;
+  savedRecipes.push(newRecipe);
+  res.send(savedRecipes);
+});
+
+apiRouter.get('/saved-recipes', (_req, res) => {
+  res.send(savedRecipes);
+});
+
+apiRouter.post('/rate', (req, res) => {
+  const recipeIndex = recipes.findIndex(recipe => recipe.name === req.body.recipeName);
+  if (recipeIndex !== -1) {
+    // Add the rating to the recipe's ratings array
+    recipes[recipeIndex].ratings.push(req.body.rating);
+    //console.log('Here');
+    // Calculate the average rating for the recipe
+    const totalRating = recipes[recipeIndex].ratings.reduce((accumulator, currentValue) => accumulator + parseFloat(currentValue), 0);
+    //console.log(totalRating);
+    console.log(recipes[recipeIndex].ratings);
+
+    //console.log(typeof totalRating);
+    const averageRating = totalRating / recipes[recipeIndex].ratings.length;
+    //console.log(averageRating);
+    recipes[recipeIndex].rating = averageRating;
+
+    res.json({ success: true, averageRating });
+  } else {
+    res.status(404).json({ success: false, message: 'Recipe not found' });
+  }
+});
+
+
+//TODO: Make sure averageRating is getting the right number and update recipe.rating to be the average rating so that it shows on the recipes page and single recipes page correctly
